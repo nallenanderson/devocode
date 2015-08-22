2e6db27e935242007edbf0c9a98af490b07434f2
Nathan Anderson <nallenanderson@gmail.com>
2015-08-22 15:41:11 -0300

    Updated Leads Mailer Text and Email Delivery Address

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,6 +1,6 @@
 class LeadMailer < ApplicationMailer
 	def new_lead(lead)
 		@lead = lead
-		mail(to: "nate@devocode.", from: @lead.email, subject: "New Contact in Devocode!")
+		mail(to: "nate@devocode.com", from: @lead.email, subject: "New Contact in Devocode!")
 	end
 end
